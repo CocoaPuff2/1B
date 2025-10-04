125868dfb3b1a28f88111385f3b10eb32ab4a054
Sana (Q) <@CocoaPuff2>
2025-10-04 12:45:00 -0700

    original

--- processes.cpp
+++ processes.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 
 int main( int argc, char** argv ) {
+    // fds[0] connect child → grandchild
+    //fds[1] connect grandchild → great-grandchild
     int fds[2][2];
     int pid;
 
@@ -21,13 +23,56 @@ int main( int argc, char** argv ) {
     if ( ( pid = fork( ) ) < 0 ) {
         perror( "fork error" );
     }
+    // child does pipeline work
     else if ( pid == 0 ) {
-        // I'm a child
+        // CHILD PROCESS
+        // create a pipe using fds[0], pipe for ps --> grep
+        pipe(fds[0]);
 
-        // create a pipe using fds[0]
-        // fork a grand-child
-        // if I'm a grand-child
-        // create a pipe using fds[1]
+        if ((pid = fork()) == 0) {
+            // GREAT GRAND-CHILD PROCESS
+            // fork a grand-child
+            // if I'm a grand-child
+            // create a pipe using fds[1]
+            pipe(fds[1]);
+
+            if ((pid = fork()) == 0) {
+                // STDIN_FILENO (0) → reads from keyboard
+                //STDOUT_FILENO (1) writes to screen
+                // 0 = read end of pipe, 1 = write end of pipe
+                dup2(fds[0][1], STDOUT_FILENO);
+                close(fds[0][0]); // closes the unused read end
+                close(fds[0][1]); // closes write
+                // -e → show all processes
+                //-f → show full format listing
+                execlp("ps", "ps", "ef", (char*)NULL);
+                perror("the exec ps command failed");
+                exit(1);
+            } else {
+                // GRAND-CHILD PROCESS
+                dup2(fds[0][0], STDIN_FILENO);
+                dup2(fds[1][1], STDOUT_FILENO);
+                close(fds[0][0]);
+                close(fds[0][1]);
+                close(fds[1][0]); // close read end (pipe1)
+                close(fds[1][1]); // close write end (pipe1)
+                execlp("grep", "grep", argv[1], (char*)NULL);
+                perror("exec grep failed");
+                exit(1);
+            }
+        } else {
+            // CHILD
+            // read from pipe1
+            dup2(fds[1][0], STDIN_FILENO);
+            close(fds[0][0]);
+            close(fds[0][1]);
+            close(fds[1][0]);
+            close(fds[1][1]);
+            execlp("wc", "wc", "-l", (char*)NULL);
+            perror("the exec wc command failed");
+            exit(1);
+        }
+
         // fork a great-grand-child
         // if I'm a great-grand-child
         // execute "ps"
@@ -37,7 +82,7 @@ int main( int argc, char** argv ) {
         // execute "wc"
     }
     else {
-        // I'm a parent
+        // PARENT
         wait( NULL );
         cout << "commands completed" << endl;
     }
